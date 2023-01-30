eddb0703de146168b2d735c78b54e194b5d08ca0
elprosdpp <sugengpratama45@gmail.com>
2023-01-30 16:25:48 +0700

    Add component header jumbotron

---
+++ components/HeaderPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="bg-[#121180]">
+    <!-- Content Header Jumbo -->
+    <div
+      class="container mx-auto p-6 flex flex-col justify-start relative z-20 pt-[10rem] pb-[6rem] overflow-hidden"
+    >
+      <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+          <li class="inline-flex items-center">
+            <a
+              href="#"
+              class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+                ></path>
+              </svg>
+              Home
+            </a>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <svg
+                aria-hidden="true"
+                class="w-6 h-6 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <a
+                href="#"
+                class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                >{{ text }}</a
+              >
+            </div>
+          </li>
+        </ol>
+      </nav>
+
+      <h1 class="text-white font-bold text-[3.5rem] lg:text-[4.5rem]">{{ text }}</h1>
+      <div class="text-[#305fb8] font-bold text-[12rem] absolute -bottom-[3rem] opacity-5">
+        {{ text }}
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<style scoped></style>
