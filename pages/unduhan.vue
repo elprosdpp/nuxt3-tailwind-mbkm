37626902e3f21f2f4ae3bcc36f4a0a173f1acd68
elprosdpp <sugengpratama45@gmail.com>
2023-01-30 14:52:40 +0700

    add page unduhan

---
+++ pages/unduhan.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- Heading Title and SEO Optimalisasi -->
+    <Head>
+      <Title>Unduhan | MBKM Universitas Ngudi Waluyo</Title>
+    </Head>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
