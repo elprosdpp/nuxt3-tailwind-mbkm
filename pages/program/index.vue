4e00665295130034de830676208c7e4b1a7ca352
elprosdpp <sugengpratama45@gmail.com>
2023-01-31 17:33:26 +0700

    add program page

---
+++ pages/program/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Heading Title and SEO Optimalisasi -->
+    <Head>
+      <Title>Program | MBKM Universitas Ngudi Waluyo</Title>
+    </Head>
+
+    <!-- Jumbotron Section -->
+    <section>
+      <HeaderPage text="Program" />
+    </section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
